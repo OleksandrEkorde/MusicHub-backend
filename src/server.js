bbe8d3338c40c948ce2ce44dc3656b1ae9cbbd67
YanaStetsenko <yanastetit@gmail.com>
2026-01-22 14:13:59 +0200

    feat: added fontend url to cors

--- src/server.js
+++ src/server.js
@@ -1,24 +1,30 @@
-import express from 'express'
-import { checkDbConnection } from './db/db.js'
-import StatsController from './controllers/StatsController.js'
+import express from "express";
+import { checkDbConnection } from "./db/db.js";
+import StatsController from "./controllers/StatsController.js";
+import cors from "cors";
 
-const app = express()
-app.use(express.json())
-app.get('/stats/users', StatsController.usersList)
-const PORT = process.env.SRV_PORT
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
+app.get("/stats/users", StatsController.usersList);
+const PORT = process.env.SRV_PORT;
 
 async function start() {
   try {
-    await checkDbConnection()
-    console.log('Успішне підключення PostgreSQL')
+    await checkDbConnection();
+    console.log("Успішне підключення PostgreSQL");
 
     app.listen(PORT, () => {
-      console.log(`Сервер запущено на порту ${PORT}`)
-    }) 
+      console.log(`Сервер запущено на порту ${PORT}`);
+    });
   } catch (err) {
-    console.error('Помилка при старті серверу', err)
-    process.exit(1)
+    console.error("Помилка при старті серверу", err);
+    process.exit(1);
   }
 }
 
-start()
+start();
