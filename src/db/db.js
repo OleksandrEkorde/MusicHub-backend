094184e15c6d99308e8d16bdd91854b7d397a07b
Your Name <you@example.com>
2026-01-30 14:08:49 +0200

    remove-log-db

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,5 @@
 import pg from 'pg'
 const { Pool } = pg
-console.log('DB_HOST:', process.env.DB_HOST)
 
 const ssl = process.env.DB_SSL === 'false' ? undefined : { rejectUnauthorized: false }
 
@@ -15,16 +14,6 @@ const pool = new Pool({
 
 export async function checkDbConnection() {
   await pool.query('SELECT 1')
-
-  const connInfo = await pool.query(
-    "select current_database() as db, current_user as user, current_setting('search_path') as search_path"
-  )
-  console.table(connInfo.rows)
-
-  const tables = await pool.query(
-    "select to_regclass('public.notes') as public_notes, to_regclass('public.musical_notes') as public_musical_notes"
-  )
-  console.table(tables.rows)
 }
 
 export default pool
